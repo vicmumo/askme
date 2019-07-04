e74116426e2857f1250809b46f77d253cf869f13
VicMo <muvictormo@gmail.com>
2019-07-04 14:26:48 +0300

    v1 landingpage

---
+++ resources/views/pub/header.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+	      <a class="navbar-brand" href="index.html">Even<span>talk.</span></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="{{ url('/')}}" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="{{ url('about')}}" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="{{ url('contact')}}"class="nav-link">Contact</a></li>
+	          <li class="nav-item cta mr-md-2"><a href="#" class="nav-link">Buy ticket</a></li>
+
+	        </ul>
+	      </div>
+	    </div>
